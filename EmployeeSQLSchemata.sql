7cf92edc98a620334c534d78ad8b45ea9091cf7b
kmharnack <kristen.harnack@gmail.com>
2020-09-23 18:21:02 -0500

    updating schema file

--- EmployeeSQLSchemata.sql
+++ EmployeeSQLSchemata.sql
@@ -1,21 +1,6 @@
 
-CREATE TABLE "departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL
-);
-
-CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL
-);
-
 CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
+    "emp_no" int   NOT NULL primary key,
     "emp_title" varchar   NOT NULL,
     "birth_date" date   NOT NULL,
     "first_name" varchar   NOT NULL,
@@ -25,11 +10,26 @@ CREATE TABLE "employees" (
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
+    "emp_no" int   NOT NULL references employees(emp_no),
     "salary" int   NOT NULL
 );
 
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL PRIMARY KEY,
+    "dept_name" varchar   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL references employees(emp_no),
+    "dept_no" varchar   NOT NULL references departments(dept_no)
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar   NOT NULL references departments(dept_no),
+    "emp_no" int   NOT NULL references employees(emp_no)
+);
+
 CREATE TABLE "titles" (
-    "title_id" varchar   NOT NULL,
+    "title_id" varchar   NOT NULL primary key,
     "title" varchar   NOT NULL
 );
